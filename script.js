262ecd566adb2ebbc69965771ef9918752a5a76f
Tunya Lénárd-Sándor <134848258+JustTunya@users.noreply.github.com>
2023-05-29 00:06:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,40 @@
-$(document).ready(function() {
-	let timeline = gsap.timeline();
+$(function() {
+	const timeline = gsap.timeline();
+	const $wrapperDivs = $('#wrapper div');
+	const $html = $('html');
+	const $window = $(window);
+	const centerx = $window.width() / 2;
+	const centery = $window.height() / 2;
   
-	$('.layer').css({ 'transform': 'translateY(100%)' });
+	$('.layer').css({ transform: 'translateY(100%)' });
   
 	timeline.staggerTo('.layer:not(.layer4, .layer5, .layer6)', 2.5, {
 	  y: '0%',
 	  ease: Power2.easeOut
-	}, 0.2);
-  
-	timeline.to('.layer4, .layer5, .layer6', 2.5, {
+	}, 0.2)
+	.to('.layer4, .layer5, .layer6', 2.5, {
 	  y: '0%',
 	  ease: Power2.easeOut
-	}, "-=2.4");
+	}, "-=2.4")
+	.eventCallback("onComplete", enableParallaxEffect)
+	.play();
   
-	timeline.eventCallback("onComplete", function() {
-	  enableParallaxEffect();
-	});
+	function enableParallaxEffect() {
+	  const wx = $window.width();
+	  const wy = $window.height();
+	  $html.mousemove(function(e) {
+		const x = e.pageX - this.offsetLeft;
+		const y = e.pageY - this.offsetTop;
+		const newx = x - centerx;
+		const newy = y - centery;
   
-	timeline.play();
-});  
+		$('span').text(newx + ", " + newy);
   
-function enableParallaxEffect() {
-	$('html').mousemove(function(e) {
-	  var wx = $(window).width();
-	  var wy = $(window).height();
-  
-	  var x = e.pageX - this.offsetLeft;
-	  var y = e.pageY - this.offsetTop;
-  
-	  var centerx = wx / 2;
-	  var centery = wy / 2;
-  
-	  var newx = x - centerx;
-	  var newy = y - centery;
-  
-	  $('span').text(newx + ", " + newy);
-  
-	  $('#wrapper div').each(function() {
-		var speed = $(this).attr('data-speed');
-		if ($(this).attr('data-revert')) speed *= -1;
-		TweenMax.to($(this), 1, { x: (1 - newx * speed), y: (1 - newy * speed) });
+		$wrapperDivs.each(function() {
+		  let speed = $(this).data('speed');
+		  if ($(this).data('revert')) speed *= -1;
+		  TweenMax.to($(this), 1, { x: (1 - newx * speed), y: (1 - newy * speed) });
+		});
 	  });
-	});
-}
-  
-  
+	}
+});
